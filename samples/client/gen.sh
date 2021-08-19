57755a91e7c4f9eef2474d81824906b0da558ae7
tweakch <alex.klee@outlook.com>
2021-08-19 22:38:30 +0200

    gen

--- samples/client/gen.sh
+++ samples/client/gen.sh
@@ -1,6 +1,21 @@
 #!/bin/sh
 # ref: https://help.github.com/articles/adding-an-existing-project-to-github-using-the-command-line/
 #
-# Usage example: /bin/sh ./gen.sh 
+# Usage example: /bin/sh ./gen.sh models
 
-java -jar swagger-codegen-cli.jar generate -i https://raw.githubusercontent.com/blockfrost/openapi/master/swagger.yaml -l csharp -c config.json -t csharp/
+args=$1
+
+rm -rf gen
+
+java -jar swagger-codegen-cli.jar generate \
+    -i https://raw.githubusercontent.com/blockfrost/openapi/master/swagger.yaml \
+    -l csharp \
+    -c ./config.json \
+    -o ./gen \
+    -t csharp 
+
+rm -f ./gen/*.sln
+dotnet new sln -n Blockfrost.Gen -o ./gen
+dotnet sln ./gen/Blockfrost.Gen.sln add ./gen/src/Blockfrost.Api/Blockfrost.Api.csproj
+dotnet sln ./gen/Blockfrost.Gen.sln add ./gen/src/Blockfrost.Api.Test/Blockfrost.Api.Test.csproj
+dotnet clean --nologo -v q
